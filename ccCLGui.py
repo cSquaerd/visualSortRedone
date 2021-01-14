4eacd4b7a9316b0be8a9534794abb178dc8be259
Charles Patterson Cook <cook.char.p@gmail.com>
2021-01-14 13:05:53 -0500

    Added lock to controls so two sorts cannot run coterminusly

--- ccCLGui.py
+++ ccCLGui.py
@@ -10,6 +10,38 @@ canvas.pack()
 
 L = cl.CanvasList(10, "maroon", "normal", canvas)
 
+def runSort(algo : str):
+	# Disable input on all controls
+	controls = (
+		delaySlider, elementsSlider,
+		randomizeButton, reverseButton,
+		bubbleSortButton, insertionSortButton,
+		shellSortButton, mergeSortButton,
+		quickSortButton, heapSortButton
+	)
+	for widget in controls:
+		widget.configure(state = tk.DISABLED)
+	# Run the sort or list method
+	if algo == "random":
+		L.randomize()
+	elif algo == "reverse":
+		L.reverse()
+	elif algo == "bubble":
+		L.bubbleSort(delaySlider.get())
+	elif algo == "insert":
+		L.insertionSort(delaySlider.get())
+	elif algo == "shell":
+		L.shellSort(delaySlider.get())
+	elif algo == "merge":
+		L.mergeSort(0, len(L.array), delaySlider.get())
+	elif algo == "quick":
+		L.quickSort(0, len(L.array) - 1, delaySlider.get())
+	elif algo == "heap":
+		L.heapSort(delaySlider.get())
+	
+	for widget in controls:
+		widget.configure(state = tk.NORMAL)
+
 delayFrame = tk.LabelFrame(base, text = "Delay on Swap or Write")
 delayFrame.grid(row = 8, column = 0)
 delaySlider = tk.Scale(
@@ -29,39 +61,45 @@ elementsSlider = tk.Scale(
 )
 elementsSlider.pack()
 
-randomizeButton = tk.Button(base, text = "Randomize", command = L.randomize)
+randomizeButton = tk.Button(
+	base, text = "Randomize",
+	command = lambda : runSort("random")
+)
 randomizeButton.grid(row = 0, column = 3)
-reverseButton = tk.Button(base, text = "Reverse", command = L.reverse)
+reverseButton = tk.Button(
+	base, text = "Reverse",
+	command = lambda : runSort("reverse")
+)
 reverseButton.grid(row = 1, column = 3)
 
 bubbleSortButton = tk.Button(
 	base, text = "Bubble Sort",
-	command = lambda : L.bubbleSort(delaySlider.get())
+	command = lambda : runSort("bubble")
 )
 bubbleSortButton.grid(row = 2, column = 3)
 insertionSortButton = tk.Button(
 	base, text = "Insertion Sort",
-	command = lambda : L.insertionSort(delaySlider.get())
+	command = lambda : runSort("insert")
 )
 insertionSortButton.grid(row = 3, column = 3)
 shellSortButton = tk.Button(
 	base, text = "Shell Sort",
-	command = lambda : L.shellSort(delaySlider.get())
+	command = lambda : runSort("shell")
 )
 shellSortButton.grid(row = 4, column = 3)
 mergeSortButton = tk.Button(
 	base, text = "Merge Sort",
-	command = lambda : L.mergeSort(0, len(L.array), delaySlider.get())
+	command = lambda : runSort("merge")
 )
 mergeSortButton.grid(row = 5, column = 3)
 quickSortButton = tk.Button(
 	base, text = "Quick Sort",
-	command = lambda : L.quickSort(0, len(L.array) - 1, delaySlider.get())
+	command = lambda : runSort("quick")
 )
 quickSortButton.grid(row = 6, column = 3)
 heapSortButton = tk.Button(
 	base, text = "Heap Sort",
-	command = lambda : L.heapSort(delaySlider.get())
+	command = lambda : runSort("heap")
 )
 heapSortButton.grid(row = 7, column = 3)
 
